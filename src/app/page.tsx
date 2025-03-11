6833b3eb9cc5b8ad92f10115868492e3c6f365c5
KevinRojas89 <kevin.rojas4268@unaula.edu.co>
2025-03-10 19:26:25 -0500

    hola

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+        <div>
+            <div>
+                
+                <a href="#">Familia Rojas</a>
+            </div>
+            <div>
+              <a href=""></a>
+              <a href=""></a>
+              <a href=""></a>
+            </div>
+        </div>
   );
 }
